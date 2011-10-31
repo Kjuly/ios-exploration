eee39c6ac0e97df264ee63c4ae9b1e81feb25bca
Kj Yu <kj@kjuly.com>
2011-10-31 17:21:14 +0800

    Add class:TableViewController | EffectTableViewPull

---
+++ EffectTableViewPull/EffectTableViewPull/TableViewController.h
@@ -0,0 +1,13 @@
+//
+//  TableViewController.h
+//  EffectTableViewPull
+//
+//  Created by Kaijie Yu on 10/31/11.
+//  Copyright (c) 2011 thePlant. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface TableViewController : UITableViewController
+
+@end
