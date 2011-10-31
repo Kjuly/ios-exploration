d4ebbd07bdc0a138e3fdc1422d7860d7142eb7f5
Kj Yu <kj@kjuly.com>
2011-10-31 17:04:27 +0800

    Initialize | EffectTableViewPull

---
+++ EffectTableViewPull/EffectTableViewPullTests/EffectTableViewPullTests.h
@@ -0,0 +1,13 @@
+//
+//  EffectTableViewPullTests.h
+//  EffectTableViewPullTests
+//
+//  Created by Kaijie Yu on 10/31/11.
+//  Copyright (c) 2011 thePlant. All rights reserved.
+//
+
+#import <SenTestingKit/SenTestingKit.h>
+
+@interface EffectTableViewPullTests : SenTestCase
+
+@end
---
+++ EffectTableViewPull/EffectTableViewPullTests/EffectTableViewPullTests.m
@@ -0,0 +1,32 @@
+//
+//  EffectTableViewPullTests.m
+//  EffectTableViewPullTests
+//
+//  Created by Kaijie Yu on 10/31/11.
+//  Copyright (c) 2011 thePlant. All rights reserved.
+//
+
+#import "EffectTableViewPullTests.h"
+
+@implementation EffectTableViewPullTests
+
+- (void)setUp
+{
+    [super setUp];
+    
+    // Set-up code here.
+}
+
+- (void)tearDown
+{
+    // Tear-down code here.
+    
+    [super tearDown];
+}
+
+- (void)testExample
+{
+    STFail(@"Unit tests are not implemented yet in EffectTableViewPullTests");
+}
+
+@end
