ed06a417a773abc975aa2bfa5b4a5e3685dc83f5
Kj Yu <kj@kjuly.com>
2011-11-09 14:14:03 +0800

    Add row check

--- Layout2RowTableView/Layout2RowTableView/TwoRowTableViewController.m
+++ Layout2RowTableView/Layout2RowTableView/TwoRowTableViewController.m
@@ -33,20 +33,20 @@
 // ---------------------------------------------------------------------------
 - (void)viewDidLoad
 {
-    [super viewDidLoad];
+  [super viewDidLoad];
 
-    // Uncomment the following line to preserve selection between presentations.
-    // self.clearsSelectionOnViewWillAppear = NO;
+  // Uncomment the following line to preserve selection between presentations.
+  // self.clearsSelectionOnViewWillAppear = NO;
  
-    // Uncomment the following line to display an Edit button in the navigation bar for this view controller.
-    // self.navigationItem.rightBarButtonItem = self.editButtonItem;
+  // Uncomment the following line to display an Edit button in the navigation bar for this view controller.
+  // self.navigationItem.rightBarButtonItem = self.editButtonItem;
 }
 // ---------------------------------------------------------------------------
 - (void)viewDidUnload
 {
-    [super viewDidUnload];
-    // Release any retained subviews of the main view.
-    // e.g. self.myOutlet = nil;
+  [super viewDidUnload];
+  // Release any retained subviews of the main view.
+  // e.g. self.myOutlet = nil;
 }
 // ---------------------------------------------------------------------------
 - (void)viewWillAppear:(BOOL)animated
@@ -93,16 +93,33 @@
 // ---------------------------------------------------------------------------
 - (UITableViewCell *)tableView:(UITableView *)tableView cellForRowAtIndexPath:(NSIndexPath *)indexPath
 {
-    static NSString *CellIdentifier = @"Cell";
+  NSLog(@"%d", [indexPath row]);
+  
+  static NSString *CellIdentifier = @"Cell";
+  
+  UIView * cellView = [[UIView alloc] init];
+  if ([indexPath row] == 0) {
+    CellIdentifier = @"CellTop";
+    [cellView setFrame:CGRectMake(0.0f, 0.0f, 300.0f, 170.0f)];
+  } else {
+    CellIdentifier = @"Cell";
+    [cellView setFrame:CGRectMake(0.0f, 0.0f, 300.0f, 100.0f)];
+  }
+  
+  UITableViewCell *cell = [tableView dequeueReusableCellWithIdentifier:CellIdentifier];
+  if (cell == nil) {
+    cell = [[[UITableViewCell alloc] initWithStyle:UITableViewCellStyleDefault reuseIdentifier:CellIdentifier] autorelease];
+  }
     
-    UITableViewCell *cell = [tableView dequeueReusableCellWithIdentifier:CellIdentifier];
-    if (cell == nil) {
-        cell = [[[UITableViewCell alloc] initWithStyle:UITableViewCellStyleDefault reuseIdentifier:CellIdentifier] autorelease];
-    }
-    
-    // Configure the cell...
-    
-    return cell;
+  // Configure the cell
+  [cellView setBackgroundColor:[UIColor colorWithWhite:0.95f alpha:1.0f]];
+  
+  // Cell Setting
+  [cell setSelectionStyle:UITableViewCellSelectionStyleNone];
+  [cell setBackgroundView:cellView];
+  [cellView release];
+  
+  return cell;
 }
 
 // ---------------------------------------------------------------------------
