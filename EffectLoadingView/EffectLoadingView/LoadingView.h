1590592b3a5443869105b1d08481585dd7d8194e
Kj Yu <kj@kjuly.com>
2011-11-11 12:01:48 +0800

    Add LoadingView class

---
+++ EffectLoadingView/EffectLoadingView/LoadingView.h
@@ -0,0 +1,13 @@
+//
+//  LoadingView.h
+//  EffectLoadingView
+//
+//  Created by Kaijie Yu on 11/11/11.
+//  Copyright (c) 2011 thePlant. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface LoadingView : UIViewController
+
+@end
